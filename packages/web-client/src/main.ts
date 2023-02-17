86b1f22fdc79ade727cec0f01c550be4360da87a
namlh <hoainamle023@gmail.com>
2023-02-17 21:45:25 +0700

    config scss variables

--- packages/web-client/src/main.ts
+++ packages/web-client/src/main.ts
@@ -4,8 +4,8 @@ import "element-plus/dist/index.css";
 import vi from "element-plus/dist/locale/vi.mjs";
 import { createPinia } from "pinia";
 import "./style.css";
-import App from "./App.vue";
-import router from "./router";
+import App from "@/App.vue";
+import router from "@/router";
 
 const app = createApp(App);
 const pinia = createPinia();
--- packages/web-client/vite.config.ts
+++ packages/web-client/vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: ` // just variables loaded globally
+          @import "./src/assets/styles";
+        `,
+      },
+    },
+  },
 });
